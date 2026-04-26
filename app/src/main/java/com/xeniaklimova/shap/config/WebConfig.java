1b8720dba938caac8d9803abb1ad8b8aff606eb4
daniel <daniel@u35.local>
2026-04-26 22:45:08 +0300

    sessions

--- app/src/main/java/com/xeniaklimova/shap/config/WebConfig.java
+++ app/src/main/java/com/xeniaklimova/shap/config/WebConfig.java
@@ -4,9 +4,11 @@ import com.hubspot.jinjava.Jinjava;
 import com.xeniaklimova.shap.view.JinjavaViewResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 import org.springframework.web.servlet.ViewResolver;
 
 @Configuration
+@EnableJdbcHttpSession
 public class WebConfig {
 
     @Bean
--- app/src/main/java/com/xeniaklimova/shap/controller/HomeController.java
+++ app/src/main/java/com/xeniaklimova/shap/controller/HomeController.java
@@ -1,9 +1,11 @@
 package com.xeniaklimova.shap.controller;
 
 import com.xeniaklimova.shap.service.StuffService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
@@ -15,10 +17,16 @@ public class HomeController {
 
 
     @GetMapping("/")
-    public String home(Model model) {
+    public String home(HttpSession session, Model model) {
         System.out.println("!!!");
         model.addAttribute("name", "Spring Boot + Jinjava");
         model.addAttribute("items", stuffService.getAll());
         return "home";
+    }
+
+    @GetMapping("/debug-session")
+    @ResponseBody
+    public String debug(HttpSession session) {
+        return session.getClass().getName();
     }
 }
